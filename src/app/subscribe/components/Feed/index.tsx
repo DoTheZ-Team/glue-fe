1eb3a0fe699f09b445a86bf1819ccb42b8901c71
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-05-24 09:29:11 +0900

    refactor: 비지니스 로직 분리를 위한 Feed 컴포넌트 리팩토링

--- src/app/subscribe/components/Feed/index.tsx
+++ src/app/subscribe/components/Feed/index.tsx
@@ -2,50 +2,12 @@
 
 import { useState } from 'react';
 import { AsyncBoundaryWithQuery } from '@/react-utils';
-import Pagination from '../Pagination';
-import FeedBox from '../FeedBox';
 import { FollowPostFetcher } from '../SubscribeFetcher';
-import { useFollowPostContext } from '../SubscribeFetcher/SubscriptionContext';
 import SubscriptionFallback from '../SubscriptionFallback';
-
-interface FollowPostProps {
-  currentPage: number;
-  onPageChange: (page: number) => void;
-}
-
-function FollowPostContent({ currentPage, onPageChange }: FollowPostProps) {
-  const { blogItemList, postItemList } = useFollowPostContext()!;
-
-  const { postItems, isFirst, isLast } = postItemList;
-  const { blogItems } = blogItemList;
-  return (
-    <div>
-      <div className="grid grid-cols-3 gap-40 py-50">
-        {postItems?.map(({ postId, title, photo, preview }, index) => {
-          const blogItem = blogItems[index];
-          return (
-            <FeedBox
-              key={postId}
-              title={title}
-              preview={preview}
-              photo={photo}
-              blogItem={blogItem}
-            />
-          );
-        })}
-      </div>
-      <Pagination
-        isFirst={isFirst}
-        isLast={isLast}
-        currentPage={currentPage}
-        onPageChange={onPageChange}
-      />
-    </div>
-  );
-}
+import FeedContent from './FeedContent';
 
 export default function Feed() {
-  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(0);
 
   return (
     <section className="flex flex-col items-start ml-380">
@@ -54,11 +16,11 @@ export default function Feed() {
         <div className="w-260 h-2 bg-primary mb-2" />
         <div className="w-270 h-2 bg-primary mb-2" />
       </div>
-      <article className="grid grid-cols-3 gap-40 py-50">
+      <article>
         <AsyncBoundaryWithQuery pendingFallback={<div>Loading...</div>}>
           <SubscriptionFallback>
             <FollowPostFetcher page={currentPage}>
-              <FollowPostContent
+              <FeedContent
                 currentPage={currentPage}
                 onPageChange={setCurrentPage}
               />
--- src/app/subscribe/components/FeedBox/index.tsx
+++ src/app/subscribe/components/FeedBox/index.tsx
@@ -4,13 +4,13 @@ import ProfileCard from '../ProfileCard';
 
 interface FeedBoxProps {
   title: string;
-  preview: string;
-  photo: string;
+  preview?: string;
+  photo?: string;
   blogItem: {
     blogId: number;
     nickname: string;
-    title: string;
-    profile: string;
+    title?: string;
+    profile?: string;
   };
 }
 
@@ -20,15 +20,24 @@ export default function FeedBox({
   photo,
   blogItem,
 }: FeedBoxProps) {
+  // FIXME: fallback이미지 수정
+  const fallbackImage = '/images/bg-temp.jpeg';
   return (
-    <div className="w-300 p-20 border border-[#b4b3b3] rounded-5">
-      <ProfileCard
-        name={blogItem.nickname}
-        title={blogItem.title}
-        profile={blogItem.profile}
-      />
+    <div className="flex flex-col w-300 p-20 border items-center border-[#b4b3b3] rounded-5 shadow cursor-pointer">
+      <div className="flex items-start justify-start w-full">
+        <ProfileCard
+          name={blogItem.nickname}
+          title={blogItem.title}
+          profile={blogItem.profile}
+        />
+      </div>
       <div className="relative w-250 h-170 p-30 mt-10 border border-[#bcbcbc] rounded-4">
-        <Image src={photo} alt="image" layout="fill" objectFit="cover" />
+        <Image
+          src={photo || fallbackImage}
+          alt="image"
+          layout="fill"
+          objectFit="cover"
+        />
       </div>
       <div className="px-5 pt-3">
         <Story title={title} content={preview} />
