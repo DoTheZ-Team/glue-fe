b916a555480b1b7fefeece2167ffc512aa17c725
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-06-07 03:49:29 +0900

    feat: searchQuery핸들링 훅 구현

---
+++ src/hooks/useSearch.tsx
@@ -0,0 +1,32 @@
+import { useState, ChangeEvent, KeyboardEvent } from 'react';
+import { useRouter } from 'next/navigation';
+
+export const useSearch = () => {
+  const [showInput, setShowInput] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const { push } = useRouter();
+
+  const handleSearchClick = () => {
+    setShowInput(!showInput);
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && searchQuery !== '') {
+      push(`/search?query=${searchQuery}`);
+    }
+  };
+
+  return {
+    showInput,
+    searchQuery,
+    handleSearchClick,
+    handleInputChange,
+    handleKeyDown,
+  };
+};
+
+export default useSearch;
