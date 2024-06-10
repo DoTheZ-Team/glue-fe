052c325f6ca2c985644fa22e9f9cba84b2a14c96
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-06-10 20:34:30 +0900

    feat: 무한스크롤 구현

---
+++ src/app/search/components/SearchFetcher/queries.ts
@@ -0,0 +1,18 @@
+// 'use clinet';
+
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { getSearchResult } from './api';
+
+export const useSearchResult = (keyword: string, size: number) => {
+  const { data, isLoading, ...rest } = useInfiniteQuery({
+    queryKey: ['search-result', keyword],
+    queryFn: ({ pageParam }) => getSearchResult(pageParam, keyword, size),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      const nextPage = allPages.length;
+      return lastPage.result.postSearchItem.hasNext ? nextPage : undefined;
+    },
+    refetchOnMount: false,
+  });
+  return { data, isLoading, ...rest };
+};
